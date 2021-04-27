28cce540c9d6e9b8884e8d0962f06ca071c6c440
seongsamin01 <80027647+seongsamin01@users.noreply.github.com>
2021-04-27 16:30:21 +0900

    Get movies query
    GraphQL MovieAPI 만들기 강의에서
    만든 MovieAPI를 가져다 씀으로써
    movie info를 가져옴.

--- src/apollo.js
+++ src/apollo.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'https://48p1r2roz4.sse.codesandbox.io',
+  uri: "http://localhost:4000/",
   cache: new InMemoryCache()
 });
 
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,3 +1,21 @@
 import React from "react";
+import { gql, useQuery } from '@apollo/client';
 
-export default () => <h1>Home</h1>;
+const GET_MOVIES = gql`
+    {
+        movies {
+            id
+            medium_cover_image
+        }
+    }
+`;
+
+export default () => {
+    const { loading, error, data } = useQuery(GET_MOVIES);
+    if(loading) {
+        return "loading...";
+    }
+    if (data && data.movies) {
+    return data.movies.map(m => <h1>{m.id}</h1>);
+    }
+};
