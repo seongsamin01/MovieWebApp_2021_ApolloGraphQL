0277235bf42be208fc339e95119282d41bd261f2
seongsamin01 <80027647+seongsamin01@users.noreply.github.com>
2021-04-28 10:58:05 +0900

    Home/Movie CSS 꾸미기
    React Apollo 는 cache를 탑재하고 있음.
    그래서 이미 들어간 페이지는 다시 새로 불러오지 않고, 기존의 페이지를 보여주는 거임.
    Redux를 사용하려면 내가 수동으로 다 해줘야 함.

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,27 +1,46 @@
 import React from "react";
 import { gql, useQuery } from '@apollo/client';
 import { useParams } from "react-router-dom";
+import styled from "styled-components";
 
 const GET_MOVIE = gql`
     query getMovie($id: Int!) {
         movie(id: $id) {
-            id
             title
+            language
+            rating
             medium_cover_image
             description_intro
         }
     }
 `;
 
+const Container = styled.div``;
+
+const Column = styled.div``;
+
+const Title = styled.h1``;
+
+const Subtitle = styled.h4``;
+
+const Description = styled.p``;
+
+const Poster = styled.div``;
+
+
 export default () => {
     const { id } = useParams();
     const { loading, data } = useQuery(GET_MOVIE, {
         variables: { id: +id },
     });
-    if (loading) {
-        return "loading";
-    }
-    if (data && data.movie) {
-        return data.movie.title;
-    }
+    return (
+        <Container>
+            <Column>
+                <Title>Name</Title>
+                <Subtitle>English ` 4.5</Subtitle>
+                <Description>hakunamatata</Description>
+            </Column>
+            <Poster></Poster>
+        </Container>
+    );
 };
