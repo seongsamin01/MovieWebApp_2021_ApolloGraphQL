e59d4891743a96776988b145edbe6065697c943c
seongsamin01 <80027647+seongsamin01@users.noreply.github.com>
2021-04-28 12:34:20 +0900

    Local State 1 - 상호작용
    Apollo cache는 한번 접속한 사이트를 기억해뒀다가 해당 데이터에서 다시 불러오는게 아니라 저장된 cache에서 불러온다.
    =======================
    사용자가 해당 데이터와 상호작용 하게 만들고 싶다면, 즉 동적 필드를 만들고 싶다면, local state를 이용

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -8,6 +8,7 @@ const GET_MOVIES = gql`
         movies {
             id
             medium_cover_image
+            isLiked @client
         }
     }
 `;
@@ -67,10 +68,14 @@ export default () => {
             {loading && <Loading>로딩 중...</Loading>}
                 <Movies>
                     {data?.movies?.map(m => (
-                        <Movie key={m.id} id={m.id} bg={m.medium_cover_image} />
+                        <Movie 
+                            key={m.id} 
+                            id={m.id}
+                            isLiked={m.isLiked} 
+                            bg={m.medium_cover_image} 
+                        />
                     ))}
                 </Movies>
-            )}
         </Container>
     );
 };
